5fde28b716b12c4fa3c74736bf918d1fd70abcda
Rafael Simionato <rafaasimi@hotmail.com>
2023-08-28 08:46:21 -0300

    Projeto: Criado resumo com os dados das vendas

--- fintech/src/pages/Resumo.tsx
+++ fintech/src/pages/Resumo.tsx
@@ -1,9 +1,54 @@
-import { useData } from "../contexts/DataContext";
+import { useData } from '../contexts/DataContext';
 
 export function Resumo() {
-  const {data} = useData();
+  const { data } = useData();
 
+  if (data === null) return null;
   return (
-    <h1>Resumo</h1>
-  )
+    <section>
+      <div className="resumo flex mb">
+        <div className="box">
+          <h2>Vendas</h2>
+          <span>
+            {data
+              .filter((i) => i.status !== 'falha')
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+          </span>
+        </div>
+
+        <div className="box">
+          <div>
+            <h2>Recebido</h2>
+            <span>
+              {data
+                .filter((i) => i.status === 'pago')
+                .reduce((acc, item) => acc + item.preco, 0)
+                .toLocaleString('pt-br', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })}
+            </span>
+          </div>
+        </div>
+
+        <div className="box">
+          <div>
+            <h2>Processando</h2>
+            <span>
+              {data
+                .filter((i) => i.status === 'processando')
+                .reduce((acc, item) => acc + item.preco, 0)
+                .toLocaleString('pt-br', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })}
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className="box">Gráficos</div>
+    </section>
+  );
 }
