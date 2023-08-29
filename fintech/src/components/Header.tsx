b9912c452f34ce6706e69af38f9014424e9ef71b
Rafael Simionato <rafaasimi@hotmail.com>
2023-08-29 08:46:33 -0300

    Projeto: Criado e estilizado sidenav

--- fintech/src/components/Header.tsx
+++ fintech/src/components/Header.tsx
@@ -1,11 +1,15 @@
+import React from 'react'
 import { DateRange } from './DateRange';
 import { Months } from './Months';
 
 export function Header() {
+  const [title, setTitle] = React.useState('Resumo');
+
   return (
     <header className='mb'>
-      <div className='mb'>
+      <div className='daterange mb'>
         <DateRange />
+        <h1 className='box bg-3'>{title}</h1>
       </div>
       <Months />
     </header>
--- fintech/src/components/Sidenav.tsx
+++ fintech/src/components/Sidenav.tsx
@@ -1,5 +1,55 @@
+import logoImg from '../assets/fintech.svg';
+import resumoIcon from '../assets/icons/resumo.svg';
+import vendasIcon from '../assets/icons/vendas.svg';
+import webhooksIcon from '../assets/icons/webhooks.svg';
+import configuracoesIcon from '../assets/icons/configuracoes.svg';
+import contatoIcon from '../assets/icons/contato.svg';
+import sairIcon from '../assets/icons/sair.svg';
+import FintechSVG from '../assets/FintechSVG';
+
 export function Sidenav() {
   return (
-    <aside className="mb">Sidenav</aside>
-  )
+    <nav className="sidenav box bg-3">
+      <FintechSVG title='Fintech'/>
+
+      <ul>
+        <li>
+          <span>
+            <img src={resumoIcon} alt="" />
+          </span>
+          <a href="">Resumo</a>
+        </li>
+        <li>
+          <span>
+            <img src={vendasIcon} alt="" />
+          </span>
+          <a href="">Vendas</a>
+        </li>
+        <li>
+          <span>
+            <img src={webhooksIcon} alt="" />
+          </span>
+          <a>Webhooks</a>
+        </li>
+        <li>
+          <span>
+            <img src={configuracoesIcon} alt="" />
+          </span>
+          <a>Configurações</a>
+        </li>
+        <li>
+          <span>
+            <img src={contatoIcon} alt="" />
+          </span>
+          <a>Contato</a>
+        </li>
+        <li>
+          <span>
+            <img src={sairIcon} alt="" />
+          </span>
+          <a>Sair</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
